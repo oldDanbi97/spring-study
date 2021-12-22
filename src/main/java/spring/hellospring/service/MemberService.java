57ef94c6e3466e7698449f469e46c4205de4afe6
minju <49747442+minjae2021@users.noreply.github.com>
2021-12-22 18:06:31 +0900

    Changes

---
+++ src/main/java/spring/hellospring/service/MemberService.java
@@ -0,0 +1,37 @@
+package spring.hellospring.service;
+
+import spring.hellospring.domain.Member;
+import spring.hellospring.repository.MemberRepository;
+import spring.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        //중복 회원은 X
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+}
