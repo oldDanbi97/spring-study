f69a61485bb4b93e8b13de59e678a407f4d532f4
minju <49747442+minjae2021@users.noreply.github.com>
2021-12-28 14:18:12 +0900

    스프링 기본 원리 공부 완료 :)

--- src/main/java/spring/hellospring/SpringConfig.java
+++ src/main/java/spring/hellospring/SpringConfig.java
@@ -1,29 +1,35 @@
 package spring.hellospring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import spring.hellospring.repository.JdbcMemberRepository;
+import spring.hellospring.aop.TimeTraceAop;
+import spring.hellospring.repository.JpaMemberRepository;
 import spring.hellospring.repository.MemberRepository;
 import spring.hellospring.service.MemberService;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JdbcMemberRepository(dataSource);
-    }
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new JpaMemberRepository(em);
+//    }
 }
---
+++ src/main/java/spring/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package spring.hellospring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import spring.hellospring.domain.Member;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
